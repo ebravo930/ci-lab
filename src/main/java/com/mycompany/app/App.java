c33f502d411f4c68d60da80a4da9a1fb386d55d6
ebravo <ebravo930@gmail.com>
2023-10-30 21:11:33 -0300

    valida numeros mayores a 0

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,37 +1,38 @@
 package com.mycompany.app;
+
 /**
  * Hello world!
  *
  */
 public class App {
-  
 
   public static String triangulo(int a, int b, int c) {
-    
-    if (a * a + b * b == c * c || b * b + c * c == a * a || c * c + a * a == b * b) {
-      return "es un triangulo pitagorico";
-  }
-  
-    if(a != b && b != c && c != a){
-      
-      return "es un triangulo escaleno";
-    }
 
-    if(a == b && b != c && c != a){
-      
-      return "es un triangulo isoceles";
-    }
+    if (a > 0 && b > 0 && c > 0) {
 
-    if (a + b <= c || b + c <= a || c + a <= b) {
-      return "error no es un triangulo";
-    }
-    if (a == b && b == c && c == a)
-    {
-      return "es un triangulo equilatero";
+      if (a * a + b * b == c * c || b * b + c * c == a * a || c * c + a * a == b * b) {
+        return "es un triangulo pitagorico";
+      }
+
+      if (a != b && b != c && c != a) {
+
+        return "es un triangulo escaleno";
+      }
+
+      if (a == b && b != c && c != a) {
+
+        return "es un triangulo isoceles";
+      }
+
+      if (a + b <= c || b + c <= a || c + a <= b) {
+        return "error no es un triangulo";
+      }
+      if (a == b && b == c && c == a) {
+        return "es un triangulo equilatero";
+      }
     }
     return "error no es un triangulo";
-  } 
-  
+  }
 
   public static void main(String[] args) {
     System.out.println("Hola Gente!");
